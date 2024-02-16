1e8ee02c698c2bf0db69985e43191ce6ee5dc008
shira2004 <shira.k4255@gmail.com>
2024-02-16 14:28:43 +0200

    Initiated new question service.

---
+++ Angular/devques/src/app/question.model.ts
@@ -0,0 +1,10 @@
+import { Category } from "./category.model"
+
+export class Question{
+    category!:Category;
+    Content!:string; 
+    //replace to answer
+    Answers!:any;
+    company!:string
+}
+
