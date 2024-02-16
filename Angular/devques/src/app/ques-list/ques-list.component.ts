4f969d423bc6a1548bddabd467264f0ad6efba62
shira2004 <shira.k4255@gmail.com>
2024-02-16 16:15:15 +0200

    Add cards with question details

--- Angular/devques/src/app/ques-list/ques-list.component.ts
+++ Angular/devques/src/app/ques-list/ques-list.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { Router } from 'express';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+
 import { QuestionService } from '../question.service';
 import { Question } from '../question.model';
+
 
 @Component({
   selector: 'app-ques-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule ,MatCardModule , MatButtonModule],
   templateUrl: './ques-list.component.html',
   styleUrl: './ques-list.component.css'
 })
@@ -19,17 +25,19 @@ export class QuesListComponent {
   constructor(private router: Router, private _quesService: QuestionService) { }
 
   ngOnInit(): void {
+    this.quesList=this._quesService.getAllQuest();
     this._quesService.getAllStudentsByServer().subscribe({
       next: (res) => {
         this.filterQuesArr = this.quesList = res
       },
       error: (err) => {
+        this.filterQuesArr=this._quesService.getAllQuest()
         console.log(err);
       }
     })
   }
 
-//what the error with navigate ?
+
 public showDetails(ques: Question) {
   this.router.navigate(["/ques/ques-details", ques.quesId]);
 }
