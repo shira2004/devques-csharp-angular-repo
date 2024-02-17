28e7a9559ced09aa2e8aee465e2150a1ef5fd199
shira2004 <shira.k4255@gmail.com>
2024-02-17 21:24:55 +0200

    Design the login component.

--- Angular/devques/src/app/log-in/log-in.component.ts
+++ Angular/devques/src/app/log-in/log-in.component.ts
@@ -1,9 +1,11 @@
-import { Input, Component, Output, EventEmitter } from '@angular/core';
+import { Input, Component, Output, EventEmitter, OnInit } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule, FormGroup} from '@angular/forms';
 import {MatIconModule} from '@angular/material/icon';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-log-in',
@@ -18,22 +20,37 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.css'
 })
-export class LogInComponent {
-  email = new FormControl('', [Validators.required, Validators.email]);
-
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
-    }
-
-    return this.email.hasError('email') ? 'Not a valid email' : '';
+export class LogInComponent implements OnInit {
+  constructor(private router: Router){}
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+    email : new FormControl('', [Validators.required, Validators.email]),
+    password :new FormControl('', [Validators.required]),
+    firstName : new FormControl('', [Validators.required]),
+    lastName : new FormControl('', [Validators.required]),
+    receiveUpdates : new FormControl(false)
+    });
   }
+  loginForm!: FormGroup;
+ 
+  // getErrorMessage() {
+  //   if (this.email.hasError('required')) {
+  //     return 'You must enter a value';
+  //   }
+
+  //   return this.email.hasError('email') ? 'Not a valid email' : '';
+  // }
   
   hide = true;
 
 
    submit() {
-   
+    console.log('hi');
+    console.log('Form Object:', this.loginForm.value);
+ 
+  }
+  signIn(){
+    this.router.navigate(['/ques']);
   }
   
 
