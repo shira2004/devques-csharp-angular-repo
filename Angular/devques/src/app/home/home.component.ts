0ed7b7e3b1dde71ab8743a5ec5257cf0bed764d0
shira2004 <shira.k4255@gmail.com>
2024-02-17 22:33:31 +0200

    Add navigation from home page

--- Angular/devques/src/app/home/home.component.ts
+++ Angular/devques/src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { LeftSectionComponent } from '../left-section/left-section.component';
 import { RightSectionComponent } from '../right-section/right-section.component';
 import { CarouselComponent } from '../carousel/carousel.component';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -11,5 +12,18 @@ import { CarouselComponent } from '../carousel/carousel.component';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  constructor(private router: Router){}
 
+  onJoinSectionClick(){
+    this.router.navigate(['sign-up']);
+  }
+  onStartSectionClick(){
+    this.router.navigate(['ques']);
+  }
+  onAboutSectionClick(){
+    this.router.navigate(['About']);
+  }
+  onAddQuesSectionClick(){
+    this.router.navigate(['add-ques']);
+  }
 }
