385161e5af23aa249f7be46e19032dfabbc7d207
shira2004 <shira.k4255@gmail.com>
2024-02-19 01:52:08 +0200

    Added company controller

---
+++ csharp/devques/CodersNest/Controllers/CompanyController.cs
@@ -0,0 +1,53 @@
+﻿using Common.Dto;
+using Microsoft.AspNetCore.Mvc;
+using Service.Interface;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DevQues.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private readonly IService<CompanyDto> _service;
+
+        public CompanyController(IService<CompanyDto> service)
+        {
+            _service = service;
+        }
+        // GET: api/<CompanyController>
+        [HttpGet]
+        public List<CompanyDto> Get()
+        {
+            return _service.GetAll();
+        }
+
+        // GET api/<CompanyController>/5
+        [HttpGet("{id}")]
+        public CompanyDto Get(int id)
+        {
+            return _service.GetById(id);
+        }
+
+        // POST api/<CompanyController>
+        [HttpPost]
+        public CompanyDto Post([FromBody] CompanyDto value)
+        {
+            return _service.Add(value);
+        }
+
+        // PUT api/<CompanyController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+
+        }
+
+        // DELETE api/<CompanyController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
