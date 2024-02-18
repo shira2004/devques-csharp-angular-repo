5ada966a5c892bbad31d387131224df88c5d36a4
shira2004 <shira.k4255@gmail.com>
2024-02-18 03:14:22 +0200

    Initiate basic question details component

--- Angular/devques/src/app/details/details.component.ts
+++ Angular/devques/src/app/details/details.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { QuestionService } from '../question.service';
+import { Question } from '../question.model';
+
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+  private questionId!:number
+
+   question1: any = {
+    quesId: 1,
+    content: 'Sample question 1',
+    user: { userId: 1, username: 'john_doe', lastName: 'Doe' },
+    date: new Date('2022-02-20'),
+    answers: [
+      { answerId: 1, content: 'Sample answer 1', user: { userId: 1, username: 'john_doe', lastName: 'Doe' }, date: new Date('2022-02-21') },
+      { answerId: 2, content: 'Sample answer 2', user: { userId: 1, username: 'john_doe', lastName: 'Doe' }, date: new Date('2022-02-22') },
+    ],
+  };
+ 
+  constructor(private route: ActivatedRoute,private _questionService: QuestionService) { }
+
+  public question!: Question
+  ngOnInit(): void {
+    this.route.params.subscribe((parm) => {
+      this.questionId = parm['id'];
+      this._questionService.getQuestByIdFromServer(this.questionId).subscribe({
+        next: (res) => {
+          this.question = res
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      })
+
+    })
+  }
 
 }
