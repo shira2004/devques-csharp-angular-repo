5cb161ba08f5185eda3f22f2db02bc5054843650
shira2004 <shira.k4255@gmail.com>
2024-02-20 23:45:55 +0200

    Added guard for user authentication.

--- Angular/devques/src/app/user.guard.ts
+++ Angular/devques/src/app/user.guard.ts
@@ -1,5 +1,17 @@
 import { CanActivateFn } from '@angular/router';
 
 export const userGuard: CanActivateFn = (route, state) => {
-  return true;
+  const userString = localStorage.getItem('user');
+  
+  if (userString) {
+    const user = JSON.parse(userString);
+    
+    if (user && typeof user === 'object') {
+      return true;
+    }
+  }
+  console.log('there is no current user connected');
+  alert('login please ')
+  
+  return false;
 };
