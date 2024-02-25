c6cdd4f645935ea604894318bfb972389f9f7656
shira2004 <shira.k4255@gmail.com>
2024-02-25 13:00:33 +0200

    Handled foreign keys and related adjustments.

--- csharp/devques/Common/Dto/AnswerDto.cs
+++ csharp/devques/Common/Dto/AnswerDto.cs
@@ -8,11 +8,14 @@ namespace Common.Dto
 {
         public class AnswerDto
         {
-            public int AnswerId { get; set; }
+      //  public int CategoryId { get; set; }
+        public int AnswerId { get; set; }
             public int QuestionId { get; set; }
             public int UserId { get; set; }
             public string Content { get; set; }
             public int Rating { get; set; }
-        
+
+        public int categoryId { get; set; }
+
     }
 }
--- csharp/devques/Common/Dto/QuestionDto.cs
+++ csharp/devques/Common/Dto/QuestionDto.cs
@@ -8,19 +8,11 @@ namespace Common.Dto
 {
         public class QuestionDto
         {
+        public int QuestionId { get; set; }
         public int CategoryId { get; set; }
-            public int QuestionId { get; set; }
-            //public  CategoryDto category { get; set; }
-             public int UserId { get; set; }
-             public int CompanyId { get; set; }
-
-        
-
-        //public int CategoryId { get; set; }
-
+       // public int UserId { get; set; }
+       public UserDto User { get; set; }
+        public int CompanyId { get; set; }
         public string Content { get; set; }
-           // public virtual ICollection<Answer> Answers { get; set; }
-            //public virtual ICollection<User> UsersMarked { get; set; }
-            //public virtual Company Company { get; set; } 
-        }
+    }
     }
