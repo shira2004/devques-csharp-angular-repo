cb3113ca82370db0f8f9cff32a070a1f3971335e
shira2004 <shira.k4255@gmail.com>
2024-02-27 17:50:54 +0200

    "Upgraded sign-up form with added checks and validation

--- Angular/devques/src/app/log-in/components/sign-up/sign-up.component.ts
+++ Angular/devques/src/app/log-in/components/sign-up/sign-up.component.ts
@@ -1,54 +1,76 @@
-import { Component, OnInit} from '@angular/core';
-import {FormControl, Validators, FormsModule, ReactiveFormsModule, FormGroup} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormsModule, ReactiveFormsModule, FormGroup } from '@angular/forms';
 
 
-import { Router } from '@angular/router';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatDialog} from '@angular/material/dialog'
+import { Router, RouterModule } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialog } from '@angular/material/dialog'
 
 
 import { UserService } from '../../../user.service';
 import { DialogComponent } from '../../../home-components/dialog/dialog.component';
 import { User } from '../../user.model';
+import { APP_ROUTES } from '../../../app_routes';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [MatFormFieldModule, 
-    MatInputModule, 
+  imports: [MatFormFieldModule,
+    MatInputModule,
     FormsModule,
     ReactiveFormsModule,
     MatIconModule,
-    MatCheckboxModule,  
-   ],
+    MatCheckboxModule,
+    RouterModule , 
+    CommonModule
+  ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent implements OnInit {
-  constructor(private router: Router , private _userService: UserService , public dialog:MatDialog  ){}
+  constructor(private router: Router, private _userService: UserService, public dialog: MatDialog) { }
   ngOnInit(): void {
     this.SignUpForm = new FormGroup({
-      firstName : new FormControl('', [Validators.required]),
-      lastName : new FormControl('', [Validators.required]),
-      email : new FormControl('', [Validators.required, Validators.email]),
-      password :new FormControl('', [Validators.required]),
-    // receiveUpdates : new FormControl(false)
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^(?=.*[a-zA-Z])(?=.*\d).{6,}$/),
+      ]),
     });
   }
-   dialogData = {
+
+  APP_ROUTES = APP_ROUTES
+  dialogData1 = {
     text1: 'Welcome!',
     text2: 'Your account has been successfully added.',
     imageSrc: '../../assets/pic/sign.gif',
     button: {
       label: 'OK',
       onClick: () => {
+        this.router.navigate([APP_ROUTES.HOME]);
+        console.log('OK button clicked');
+
+      },
+    },
+  };
+
+  dialogData2 = {
+    text1: 'ooppss!',
+    text2: 'this account is already exist.',
+    imageSrc: '../../assets/pic/error.gif',
+    button: {
+      label: 'OK',
+      onClick: () => {
         this.router.navigate(['home']);
         console.log('OK button clicked');
-       
+
       },
     },
   };
@@ -57,18 +79,26 @@ export class SignUpComponent implements OnInit {
   submit() {
     console.log('Form Object:', this.SignUpForm.value);
     const userToAdd: User = this.SignUpForm.value;
-  
+    console.log(userToAdd);
+
     this._userService.addUserByServer(userToAdd).subscribe({
       next: (res) => {
         console.log(res);
         localStorage.setItem('user', JSON.stringify(res))
         this.dialog.open(DialogComponent, {
-          data: this.dialogData
+          data: this.dialogData1
         });
       },
       error: (err) => {
         console.log(err);
+        this.dialog.open(DialogComponent, {
+          data: this.dialogData2
+        });
       }
     });
+  }
+  GoToSignIn() {
+    console.log('go to sign in ');
+    this.router.navigate([APP_ROUTES.SIGN_IN]);
   }
 }  
