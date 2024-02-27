1bb918d95c29db22697263c8009cc2af3232b39d
shira2004 <shira.k4255@gmail.com>
2024-02-27 17:38:33 +0200

    Updated model - added user to questions.

--- Angular/devques/src/app/answer.model.ts
+++ Angular/devques/src/app/answer.model.ts
@@ -1,9 +1,24 @@
+import { User } from './log-in/user.model';
 export class Answer{
     content!: string 
     answerId!:number
     userId!:number
+    user?: User
     questionId! :number
     rating!:0
+    categoryId!:number
+    code?:string
+
+    
 }
 
+
+
+    
+   
+  
+   
+ 
+    
+  
 
