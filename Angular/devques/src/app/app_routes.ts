8c63382cd5f9b4efc440e6e1e63832e6d98c691b
shira2004 <shira.k4255@gmail.com>
2024-02-27 17:47:05 +0200

    Added app-routes for comprehensive routing prevention.

---
+++ Angular/devques/src/app/app_routes.ts
@@ -0,0 +1,10 @@
+export const APP_ROUTES = {
+    SIGN_IN: "/sign-in",
+    SIGN_UP: "/sign-up",
+    ABOUT: "/About",
+    CATEGORY_SELECTIONS: "/category-Selections",
+    ADD_QUEST: "/ques/add-ques",
+    QUES_LIST:"/ques/ques-list" ,
+    HOME: 'home',
+    DETAILS: '/ques/ques-details'
+}
