fe75a2f307979e80aff9343b1bb2ac1b0ffdae03
shira2004 <shira.k4255@gmail.com>
2024-02-27 17:40:13 +0200

    Added answer component.

---
+++ Angular/devques/src/app/answers/answers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-answers',
+  standalone: true,
+  imports: [],
+  templateUrl: './answers.component.html',
+  styleUrl: './answers.component.css'
+})
+export class AnswersComponent {
+
+}
