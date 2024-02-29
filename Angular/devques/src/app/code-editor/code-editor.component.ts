b9a71cda0bce2f97a883e32e128c79f07958ae33
shira2004 <shira.k4255@gmail.com>
2024-02-29 22:29:48 +0200

    Added code editor component.

---
+++ Angular/devques/src/app/code-editor/code-editor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-code-editor',
+  standalone: true,
+  imports: [],
+  templateUrl: './code-editor.component.html',
+  styleUrl: './code-editor.component.css'
+})
+export class CodeEditorComponent {
+
+}
