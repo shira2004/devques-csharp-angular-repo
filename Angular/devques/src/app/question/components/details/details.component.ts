80a3af2bd4551025242e060127496c4933360545
shira2004 <shira.k4255@gmail.com>
2024-03-09 19:13:24 +0200

    "Added content to the 'Details' component, utilized 'helper.ts' for image addition.

--- Angular/devques/src/app/question/components/details/details.component.ts
+++ Angular/devques/src/app/question/components/details/details.component.ts
@@ -8,24 +8,25 @@ import { MatDialog } from '@angular/material/dialog';
 import { APP_ROUTES } from '../../../app_routes';
 import { DialogComponent } from '../../../home-components/dialog/dialog.component';
 import { Answer } from '../../../models/answer.model';
+import { handleFileSelected } from '../../../helper';
 
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrl: './details.component.css' 
+  styleUrl: './details.component.css'
 })
 export class DetailsComponent implements OnInit {
-// convertToBlobUrl(arg0: string|undefined): File {
-// throw new Error('Method not implemented.');
-// }
+  // convertToBlobUrl(arg0: string|undefined): File {
+  // throw new Error('Method not implemented.');
+  // }
   public question: Question | null = null;
   public filteredAnswers: Answer[] = [];
   showTextField = false;
   showCodeField = false;
   comment: string = '';
   code: string = '';
-  public ans!:Answer
+  public ans!: Answer
   public userId!: number;
   isDarkMode = true;
   selectedImage: File | null = null;
@@ -39,7 +40,7 @@ export class DetailsComponent implements OnInit {
     private el: ElementRef,
     public dialog: MatDialog,
     private router: Router
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     var currentUserString = localStorage.getItem('user');
@@ -57,15 +58,15 @@ export class DetailsComponent implements OnInit {
       if (this.question) {
         this.filteredAnswers = allAnswers.filter(answer => answer.questionId === this.question?.questionId);
       }
-      
+
     });
   }
-  
+
   APP_ROUTES = APP_ROUTES
   dialogData1 = {
     text1: 'thank you !',
     text2: 'Your answer has been successfully added.',
-    imageSrc: '../../assets/pic/sign.gif',
+    imageSrc: '../../assets/pic/coming.gif',
     button: {
       label: 'OK',
       onClick: () => {
@@ -97,61 +98,81 @@ export class DetailsComponent implements OnInit {
   toggleCodeField() {
     console.log('toggling code field');
     console.log(this.showCodeField);
-    
+
     this.showCodeField = !this.showCodeField;
     console.log(this.showCodeField);
-    
+
   }
   toggleDarkMode() {
     this.isDarkMode = !this.isDarkMode;
-}
+  }
 
   onSubmit() {
     console.log('submit func');
-    
+
     this.ans = {
       answerId: 0,
       questionId: this.question?.questionId || 0,
       userId: this.userId,
       content: this.comment,
-      code:this.code,
+      code: this.code,
       rating: 0,
       categoryId: this.question?.categoryId || 0,
       image: this.base64Image || '',
-      
+      score: 0
+
     };
     console.log(this.ans);
-    
-  
+
+
     this._ansService.addAnswer(this.ans).subscribe({
       next: (res) => {
         console.log(res);
         this.dialog.open(DialogComponent, {
-          data: this.dialogData1
+          data: this.dialogData1,
+          height: '500px',
+          width: '300px',
         });
       },
       error: (err) => {
         console.log('😢');
-        
+
         console.log(err);
         this.dialog.open(DialogComponent, {
-          data: this.dialogData2
+          data: this.dialogData2,
+          height: '500px',
+          width: '300px',
         });
       }
     });
   }
 
   onFileSelected(event: any): void {
-    this.selectedImage = event.target.files[0];
-    if (this.selectedImage) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        this.base64Image = e.target?.result as string;
-      };
-      reader.readAsDataURL(this.selectedImage);
-    }
+    handleFileSelected(event, (base64Image) => {
+      this.selectedImage = event.target.files[0];
+      this.base64Image = base64Image;
+     
+    });
   }
 
- 
-  
+  copyText(val: string) {
+    let selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+  }
+
+
+  prev() {    
+    this._quesService.isArrow = true; 
+    this.router.navigate([APP_ROUTES.QUES_LIST]);
+  }
+
 }
